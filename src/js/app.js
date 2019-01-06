9a07f37a65a59db8830a0a9771137ba5b2b0689c
AvBorikDeveloper <avboris28@gmail.com>
2019-01-06 21:15:10 +1300

    Fixed the problem with yarn, stuck on 95%, should have public path setted, now all assests building fine

---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+console.log('Suka it is works!!!!');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,1 +1,7 @@
 const { mix } = require('laravel-mix');
+
+
+ mix.sass('src/sass/app.scss', 'dist/css')
+    .js('src/js/app.js', 'dist/js')
+    .sourceMaps()
+    .setPublicPath('dist');
